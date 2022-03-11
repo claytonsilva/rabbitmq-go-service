cbe244aeffe61ec8b902b0bca3e0d0a5780c0a4a
claytonsilva <clayton.silva@grupoboticario.com.br>
2022-03-11 11:58:18 -0300

    feat: added option to choose payload size to send

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -21,19 +21,27 @@ func main() {
 	EnvCmd := os.Getenv("CMD")
 	EnvInterval, err := strconv.Atoi(os.Getenv("INTERVAL"))
 	failOnError(err, fmt.Sprintf("cannot convert %s to int", os.Getenv("INTERVAL")), false)
+	failOnError(err, fmt.Sprintf("cannot convert %s to int", os.Getenv("PAYLOAD_SIZE")), false)
 	if EnvInterval == 0 {
 		// set default value in error case
 		EnvInterval = 1000
+	}
+	EnvPayloadSize, err := strconv.Atoi(os.Getenv("PAYLOAD_SIZE"))
+	failOnError(err, fmt.Sprintf("cannot convert %s to int", os.Getenv("PAYLOAD_SIZE")), false)
+	if EnvPayloadSize == 0 {
+		// set default value in error case
+		EnvPayloadSize = 128
 	}
 
 	// using standard library "flag" package
 	cmd := flag.String("cmd", EnvCmd, "cmd to send")
 	interval := flag.Int("interval", EnvInterval, "time window to call inner cmd")
+	payloadSize := flag.Int("size", EnvPayloadSize, "time window to call inner cmd")
 	flag.Parse()
 
 	log.Printf("running %v cmd", cmd)
 	if *cmd == "send" {
-		send(EnvAMPQQueryString, EnvQueueName, *interval)
+		send(EnvAMPQQueryString, EnvQueueName, *interval, *payloadSize)
 	} else {
 		receive(EnvAMPQQueryString, EnvQueueName, *interval)
 	}
